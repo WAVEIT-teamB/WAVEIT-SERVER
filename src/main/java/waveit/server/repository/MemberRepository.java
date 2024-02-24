6f104c0bb246732c971768474fa6186192456b16
se0hyun <kimlisa00@seoultech.ac.kr>
2024-02-25 04:06:03 +0900

    FEAT: MemberRepository

--- src/main/java/waveit/server/repository/MemberRepository.java
+++ src/main/java/waveit/server/repository/MemberRepository.java
@@ -3,8 +3,12 @@ package waveit.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import waveit.server.domain.Member;
+
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
-    Member findByLoginIdAndPassword(String loginId, String password);
 }
